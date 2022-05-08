cf4e90098481f3b7e57fb3c75254c94a33492e28
sragu2000 <s.s.raguraj@gmail.com>
2022-05-08 21:42:23 +0530

    flame

---
+++ FullCode/headerfiles.h
@@ -0,0 +1,7 @@
+#include <avr/io.h>
+#include "registerFunctions.h"
+#include <util/delay.h>
+
+//pin definitions
+#define flame 7
+#define led 0
--- FullCode/main.c
+++ FullCode/main.c
@@ -1,9 +1,12 @@
-#include <avr/io.h>
-
-
+#define F_CPU 8000000UL
+#include "headerfiles.h"
 int main(void){
-    while (1){
-		
-    }
+	setOutput(PORTC,led);
+	while (1){
+		if(pinRead(PINC,flame)){
+			portHigh(PORTC,led);
+		}else{
+			portLow(PORTC,led);
+		}
+	}
 }
-

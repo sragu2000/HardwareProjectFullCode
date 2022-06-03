fc8682d993db211bea77b7ac044ec77df9fd046e
sragu2000 <s.s.raguraj@gmail.com>
2022-06-03 08:07:01 +0530

    c

--- FullCode/pwm.h
+++ FullCode/pwm.h
@@ -5,6 +5,7 @@
  * Created: 5/22/2022 3:17:18 PM
  *  Author: ssrag
  */ 
+//fast pwm mode
 void PWM_init(){
 	/*set fast PWM mode with non-inverted output*/
 	TCCR0 = (1<<WGM00) | (1<<WGM01) | (1<<COM01) | (1<<CS00);
